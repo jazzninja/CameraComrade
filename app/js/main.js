5321e7d765de204040f60e611b780776dcecf496
Ryan Lee <ryan_lee@putnam.com>
2016-10-11 16:11:33 -0400

    backbone add

--- app/js/main.js
+++ app/js/main.js
@@ -1,43 +1,73 @@
-window.onload = function() { init();};
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0AmYzu_s7QHsmdE5OcDE1SENpT1g2R2JEX2tnZ3ZIWHc&output=html';
+        
+      /* 
+        You need to declare the tabletop instance separately, then feed it into the model/collection
+        You *must* specify wait: true so that it doesn't try to fetch when you initialize
+      */
+var storage = Tabletop.init( { key: public_spreadsheet_url,
+                                wait: true } )
 
-var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1jspbTfijbvU7FUhICp6rXZfOXs6avxpBifLdxoYXj-E/pubhtml';
+      /*
+       Need to specify that you'd like to sync using Backbone.tabletopSync
+       Can specify tabletop attributes, or you can do it on the collection
+      */
+var Cat = Backbone.Model.extend({
+  idAttribute: 'Name',
+  tabletop: {
+    instance: storage,
+    sheet: 'Cats'
+  },
+  sync: Backbone.tabletopSync
+})
 
-function init() {
-	document.getElementById('splash').style.display = 'block'; 
-}
-
-function showInfo(data, tabletop) {
-  	var source   = $("#cam-template").html();
-  	var template = Handlebars.compile(source);
-
-    $.each( tabletop.sheets("DucaCamChart").all(), function(i, cam) {
-    	var html = template(cam);
-        $("#content").append(html);
-    });
-}
-
-function SplashBeGone() { 
-  $('#splash').fadeOut(600);
-  Tabletop.init( { key: public_spreadsheet_url,
-  					callback: showInfo,
-  					simpleSheet: true} );
-};
-
-
-$("#search-btn").click(function(){
-	var hitTemplate = Handlebars.compile($("#cam-template").html());
-
-  	$("#content").empty();
-  	$.getJSON($("#tableFilter").val() + "&wt=json&json.wrf=?&indent=true", function(result){
-    	for (var i = 0; i < result.response.docs.length; i++) {
-      	$("#content").append(hitTemplate({title: result.response.docs[i].title, text: result.response.docs[i].text}));
-    	}
-  	});
+      /*
+       Need to specify that you'd like to sync using Backbone.tabletopSync
+       Need to specify a tabletop key and sheet
+      */
+var CatCollection = Backbone.Collection.extend({
+          // Reference to this collection's model.
+    model: Cat,
+    tabletop: {
+      instance: storage,
+      sheet: 'Cats'
+    },
+    sync: Backbone.tabletopSync
 });
 
-//document.getElementById("button-showinfo").onclick = showInfo();
-/*document.getElementById("button-show-info").on("click", showInfo());*/
+var CatView = Backbone.View.extend({
+    tagname: 'div',
+    template: _.template($('#cat-template').html()),
 
+    render: function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    }
+})
 
+$(document).ready( function() {
+  var cats = new CatCollection();
+  cats.fetch({ success: showInfo });
+});
+      
+function showInfo(cats) {
+  var henry_view = new CatView({ model: cats.get('Henry') });
 
+  $("#content").append( henry_view.render().el );
 
+  var bosco_view = new CatView({ model: cats.get('Bosco') });
+
+  $("#content").append( bosco_view.render().el );
+        
+        /*
+          NOT WORKING, NOT SURE WHY.
+          
+          Fetching on models works as long as you've specified a sheet
+          and an idAttribute for the Backbone.Model (you can always
+          use rowNumber, it comes baked in to Tabletop)
+        */
+  thomas = new Cat({name: 'Thomas'})
+  thomas.fetch();
+
+  var thomas_view = new CatView({ model: thomas });
+  $("#content").append( thomas_view.render().el );
+}
